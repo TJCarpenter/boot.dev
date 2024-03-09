c8e401a43270632449ef2f4f9c117c6166472648
Tyler Carpenter <tcarpent@byrider.com>
2024-03-09 14:41:57 -0500

    Add word count and reporting

--- bookbot/main.py
+++ bookbot/main.py
@@ -1,4 +1,22 @@
 file = "./books/frankenstein.txt"
 
-with open(file) as f:
-    print(f.read())
+def read(file):
+    with open(file) as f:
+        return f.read()
+
+def count_letters(string):
+    res = {}
+    for char in string.lower():
+        if char not in res:
+            res[char] = 0
+        res[char] += 1
+    return res
+
+def print_report(obj):
+    for key in sorted(obj, key=obj.get, reverse=True):
+        if key.isalpha():
+            print(f"The '{key}' key was found {obj[key]} times")
+
+              
+count = count_letters(read(file))
+print_report(count)
